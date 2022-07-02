2592df5b502bdba8d0c54a1e02fa63ee6ae9a5ab
SanCross <107788137+SanCross@users.noreply.github.com>
2022-07-02 12:01:59 +0700

    first commit

--- src/plesiranoke/TambahTokoController.java
+++ src/plesiranoke/TambahTokoController.java
@@ -1,26 +1,88 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package plesiranoke;
 
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.StaxDriver;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
-/**
- * FXML Controller class
- *
- * @author Asus ROG
- */
+//import class DaftarToko
+import Model.DaftarToko;
+import Model.ArrayList;
+
 public class TambahTokoController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    ArrayList<DaftarToko> daftarToko = new ArrayList<DaftarToko>();
+
+    @FXML
+    private TextField tfNamaToko;
+
+    @FXML
+    private TextField tfAlamat;
+
+    @FXML
+    private TextField tfEmail;
+
+    @FXML
+    private TextField tfNoTelfon;
+
+    XStream xstream = new XStream(new StaxDriver());
+
+    void openTabel() {
+        FileInputStream berkasMasuk;
+        try {
+            berkasMasuk = new FileInputStream("berkas2.xml");
+            int isi;
+            char c;
+            String s = "";
+            while ((isi = berkasMasuk.read()) != - 1) {
+                c = (char) isi;
+                s = s + c;
+            }
+            daftarToko = (ArrayList<DaftarToko>) xstream.fromXML(s);
+            berkasMasuk.close();
+        } catch (Exception e) {
+            System.out.println("Terjadi kesalahan: " + e.getMessage());
+        }
+    }
+
+    void simpanData() {
+        String xml = xstream.toXML(daftarToko);
+        FileOutputStream berkasKeluar;
+
+        try {
+            byte[] info = xml.getBytes("UTF-8");
+            berkasKeluar = new FileOutputStream("berkas2.xml");
+            berkasKeluar.write(info);
+            berkasKeluar.close();
+
+        } catch (Exception io) {
+            System.out.println("Terjadi kesalahan : " + io.getMessage());
+        }
+    }
+
+    @FXML
+    private void unggah(ActionEvent event) {
+        XStream xstream = new XStream(new StaxDriver());
+        String namaToko = tfNamaToko.getText();
+        String noTelfon = tfNoTelfon.getText();
+        String alamat = tfAlamat.getText();
+        String email = tfEmail.getText();
+
+        openTabel();
+
+        daftarToko.add(new DaftarToko(namaToko, alamat, email, noTelfon));
+        
+        simpanData();
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        openTabel();
+    }
 }
--- src/plesiranoke/TambahWisataController.java
+++ src/plesiranoke/TambahWisataController.java
@@ -1,26 +1,75 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package plesiranoke;
 
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.StaxDriver;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
-/**
- * FXML Controller class
- *
- * @author Asus ROG
- */
+import Model.ArrayList;
+import Model.DaftarPostingan;
+
 public class TambahWisataController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    ArrayList<DaftarPostingan> data = new ArrayList<DaftarPostingan>();
+
+    @FXML
+    private TextField tfJudul;
+
+    @FXML
+    private TextField tfDeskripsi;
+
+    void openTabel() {
+        XStream xstream = new XStream(new StaxDriver());
+        FileInputStream berkasMasuk;
+        try {
+            berkasMasuk = new FileInputStream("berkas.xml");
+            int isi;
+            char c;
+            String s = "";
+            while ((isi = berkasMasuk.read()) != - 1) {
+                c = (char) isi;
+                s = s + c;
+            }
+            data = (ArrayList<DaftarPostingan>) xstream.fromXML(s);
+            berkasMasuk.close();
+        } catch (Exception e) {
+            System.out.println("Terjadi kesalahan: " + e.getMessage());
+        }
+    }
+
+    @FXML
+    private void unggah(ActionEvent event) {
+        XStream xstream = new XStream(new StaxDriver());
+        String judul = tfJudul.getText();
+        String deskripsi = tfDeskripsi.getText();
+        
+        openTabel();
+
+        data.add(new DaftarPostingan(judul, deskripsi));
+
+        String xml = xstream.toXML(data);
+        FileOutputStream berkasKeluar;
+
+        try {
+            byte[] info = xml.getBytes("UTF-8");
+            berkasKeluar = new FileOutputStream("berkas.xml");
+            berkasKeluar.write(info);
+            berkasKeluar.close();
+
+        } catch (Exception io) {
+            System.out.println("Terjadi kesalahan : " + io.getMessage());
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        openTabel();
+    }
+
 }
