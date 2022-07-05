e922e3c01c52d4eb35bd84297c4765aacf615d48
SanCross <107788137+SanCross@users.noreply.github.com>
2022-07-05 16:33:19 +0700

    first commit

--- src/plesiranoke/Plesiranoke.java
+++ src/plesiranoke/Plesiranoke.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Plesiranoke extends Application {
@@ -11,6 +12,7 @@ public class Plesiranoke extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/View/halamanAwal.fxml"));
+        stage.getIcons().add(new Image("gambar/Plesiran (2).png"));
         stage.setTitle("Plesiran");
         stage.setScene(new Scene(root));
         stage.show();
